34a990a0841653a47a7ae8d303453cff6bb9d338
Martin Volerich <martin@volerich.com>
2010-10-24 08:59:25 -0400

    Working example code

---
+++ app/models/survey.rb
@@ -0,0 +1,8 @@
+class Survey < ActiveRecord::Base
+  
+  has_many :questions, :dependent => :destroy
+  
+  accepts_nested_attributes_for :questions, :reject_if => lambda { |a| a[:content].blank? },
+    :allow_destroy => true
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Complexforms::Application.config.secret_token = 'f46f29131458702ac92aec9bdd97f3448be5c1ce9cf0ff6269a9c8969fe7f6b4b70eafe8e1b0f11408579378491e7a7628c4c1d6f3bef5fac92b437e441c2e81'
